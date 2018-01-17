b3fc02ed6e1da79f6cd435beeb99501c917de80c
Andrii <andrii.belon@gmail.com>
2018-01-17 22:52:10 +0200

    Add multithreading with Pool

--- DZ_call_outer_programs/multi_process_with_pool.py
+++ DZ_call_outer_programs/multi_process_with_pool.py
@@ -2,18 +2,16 @@ import os
 import multiprocessing
 import subprocess
 
-def resize_file(file_name, source_dir, result_dir):
+abs_dir_path = os.path.abspath(os.path.dirname(__file__))
+source_dir = os.path.join(abs_dir_path, 'Source')
+result_dir = os.path.join(abs_dir_path, 'Result')
+
+def resize_file(file_name):
     print('Process {} has started!'.format(os.getpid()))
     command = 'cp {} {}'.format(os.path.join(source_dir, file_name), os.path.join(result_dir, file_name))
     subprocess.call(command, shell=True)
     subprocess.call('sips --resampleWidth 200 {}'.format(os.path.join(result_dir, file_name)), shell=True)
     print('Process {} ends!'.format(os.getpid()))
 
-
-abs_dir_path = os.path.abspath(os.path.dirname(__file__))
-source_dir = os.path.join(abs_dir_path, 'Source')
-result_dir = os.path.join(abs_dir_path, 'Result')
-
-for file_name in os.listdir(source_dir):
-    p1 = multiprocessing.Process(target=resize_file, args=(file_name, source_dir, result_dir))
-    p1.start()
+with multiprocessing.Pool(processes=4) as pool:
+    pool.map(resize_file, os.listdir(source_dir))
